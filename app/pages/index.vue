427b5fad8c8ce9bdf29d9d72082d61de9ce226b6
Rodrigo <rodrigobaesa@gmail.com>
2026-05-21 18:43:33 -0300

    feat: add card to display modules

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,31 @@
 <script setup>
 definePageMeta({
-    layouts: 'default'
+    layout: 'default',
+    // middleware: 'auth'
 })
+
+const modules = [
+    {
+        name: 'Reading Notes',
+        description: 'Learn how to read notes and the basics about clefs and staves.',
+        action: 'Start'
+    },
+    {
+        name: 'Reading Notes',
+        description: 'Learn how to read notes and the basics about clefs and staves.',
+        action: 'Start'
+    }
+]
 </script>
 
-<template>    
+<template>
+    <div>
+        <div v-for="module in modules" :key="module.name">
+            <UCard :title="module.name" :description="module.description" class="mb-5">
+                <NuxtLink to="/module">
+                    <UButton icon="i-lucide-music">Start</UButton>
+                </NuxtLink>
+            </UCard>
+        </div>
+    </div>
 </template>
