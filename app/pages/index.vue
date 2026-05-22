1009156b2721f549503daa4c69115631b24bb191
Rodrigo <rodrigobaesa@gmail.com>
2026-05-22 15:29:23 -0300

    feat: add dynamic route to module

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,21 +8,24 @@ const modules = [
     {
         name: 'Reading Notes',
         description: 'Learn how to read notes and the basics about clefs and staves.',
-        action: 'Start'
+        action: 'Start',
+        path: 'reading-notes'
     },
     {
-        name: 'Reading Notes',
+        name: 'Reading Notes 2',
         description: 'Learn how to read notes and the basics about clefs and staves.',
-        action: 'Start'
+        action: 'Start',
+        path: 'reading-notes-2'
+
     }
 ]
 </script>
 
-<template>
+<template>    
     <div>
-        <div v-for="module in modules" :key="module.name">
+        <div v-for="module in modules" :key="module.path">
             <UCard :title="module.name" :description="module.description" class="mb-5">
-                <NuxtLink to="/module">
+                <NuxtLink :to="`/modules/${module.path}`">
                     <UButton icon="i-lucide-music">Start</UButton>
                 </NuxtLink>
             </UCard>
---
+++ app/pages/modules/[moduleId]/index.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { TimelineItem } from '@nuxt/ui'
+
+const route = useRoute()
+
+const moduleId = route.params.moduleId
+
+
+definePageMeta({
+    layout: 'default'
+})
+
+const items = ref<TimelineItem[]>([
+    {
+        title: 'Module 1',
+        description: 'Learn about staves and clefs.',
+        icon: 'i-lucide-music'
+    },
+    {
+        title: 'Module 1',
+        description: 'Learn about staves and clefs.',
+        icon: 'i-lucide-music'
+    },
+])
+</script>
+
+<template>
+    <h1>{{ moduleId }}</h1>
+    <UTimeline :items="items" />
+</template>
